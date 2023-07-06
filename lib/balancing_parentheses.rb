dd0aff0012dfb31d0efc566ecef8d8eb6bdc0695
Samuel Amity <sam.amity@gmail.com>
2023-07-06 16:29:28 -0400

    lab completed

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,18 @@
 require_relative './stack'
 
-# your code here
+def balancing_parentheses(string)
+    @stack = Stack.new
+    string.chars.each do |char|
+        if @stack.size == 0
+            @stack.push(char)
+        else
+            if @stack.peek == ")" || char == "("
+                @stack.push(char)
+            else
+                @stack.pop
+            end 
+        end
+    end
+
+    @stack.size
+end
